5a0e4ca3afb0c72439e570152ed1ca5d42a2cda8
bippolaroid <bippolaroid@proton.me>
2025-07-29 01:53:54 -0400

    favicon and 404 catch-all

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,24 @@
-import { Router } from "@solidjs/router";
+import { Route, Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
+import { MetaProvider, Title } from "@solidjs/meta";
+import Home from "./routes";
 
 export default function App() {
   return (
     <Router
-      root={props => (
+      root={(props) => (
         <>
           <Suspense>{props.children}</Suspense>
         </>
       )}
     >
+      <MetaProvider>
+        <Title>EDGAR - Creative Studio in New Jersey</Title>
+      </MetaProvider>
       <FileRoutes />
+      <Route path="*404" component={Home} />
     </Router>
   );
 }
--- src/routes/[...404].tsx
+++
@@ -1,8 +0,0 @@
-
-export default function NotFound() {
-  return (
-    <main class="mx-auto text-center mt-[50vh]">
-      Not Found
-    </main>
-  );
-}
